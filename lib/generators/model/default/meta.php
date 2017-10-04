e839cef9a85bbc5d9321da61a766c1497a428e07
Kozhin Vladimir <affka@affka.ru>
2017-10-04 11:06:56 +0700

    fix redux actions import

--- lib/generators/model/default/meta.php
+++ lib/generators/model/default/meta.php
@@ -89,8 +89,8 @@ if ($metaItem->publishToFrontend) {?>
 <?php if ($relation->isHasOne || $relation->isHasMany) { ?>
         return $this-><?= $relation->type ?>(<?= $relation->relationClass->name ?>::className(), ['<?= $relation->relationKey ?>' => '<?= $relation->selfKey ?>']);
 <?php } elseif ($relation->isManyMany) { ?>
-        return $this->hasMany(<?= $relation->relationClass->name ?>::className(), ['<?= $relation->relationKey ?>' => '<?= $relation->viaSelfKey ?>'])
-            ->viaTable('<?= $relation->viaTable ?>', ['<?= $relation->viaRelationKey ?>' => '<?= $relation->selfKey ?>']);
+        return $this->hasMany(<?= $relation->relationClass->name ?>::className(), ['<?= $relation->relationKey ?>' => '<?= $relation->viaRelationKey ?>'])
+            ->viaTable('<?= $relation->viaTable ?>', ['<?= $relation->viaSelfKey ?>' => '<?= $relation->selfKey ?>']);
 <?php } ?>
     }
 <?php } ?>
